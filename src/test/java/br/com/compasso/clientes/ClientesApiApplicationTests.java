377e7b5a2e8a509d89609b561d53ce77cd777a28
jovanibrasil <jovanibrasil@gmail.com>
2020-04-16 11:37:56 -0300

    Adiciona projeto inicial. Closes #1

---
+++ src/test/java/br/com/compasso/clientes/ClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
