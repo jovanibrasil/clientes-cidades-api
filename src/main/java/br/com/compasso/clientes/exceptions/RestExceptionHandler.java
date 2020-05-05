1d1a5c01653bcd1798f8e3c1a0db72bf273374db
jovanibrasil <jovanibrasil@gmail.com>
2020-05-05 16:41:47 -0300

    Adiciona mais detalhes ao retorno do handler de exceptions. Closes #24

--- src/main/java/br/com/compasso/clientes/exceptions/RestExceptionHandler.java
+++ src/main/java/br/com/compasso/clientes/exceptions/RestExceptionHandler.java
@@ -1,14 +1,19 @@
 package br.com.compasso.clientes.exceptions;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -16,20 +21,49 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private static final Logger log = LoggerFactory.getLogger(RestExceptionHandler.class);
 	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<ErroValidacao> fieldErrors = ex.getBindingResult().getFieldErrors()
+				.stream().map(e -> new ErroValidacao(e.getDefaultMessage(),
+				e.getField(), e.getRejectedValue())).collect(Collectors.toList());
+
+		DetalheErro<List<ErroValidacao>> errorDetail = new DetalheErro.Builder<List<ErroValidacao>>()
+				.message("Valores inválidos de entrada")
+				.code(status.value())
+				.status(status.getReasonPhrase())
+				.timestamp(LocalDateTime.now())
+				.objectName(ex.getBindingResult().getObjectName())
+				.errors(fieldErrors).build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetail);
+	}
+	
 	@ResponseStatus(code = HttpStatus.NOT_FOUND)
 	@ExceptionHandler({ NotFoundException.class })
-	public List<String> handleNotFound(Exception exception) {
+	public DetalheErro<String> handleNotFound(Exception exception) {
+		
 		String message = exception.getMessage();
-		log.info("Uma exception foi capturada {}", message);
-		return Arrays.asList(message);
+		log.info("Uma NotFoundException foi capturada {}", message);
+		
+		return new DetalheErro.Builder<String>()
+				.message(message)
+				.timestamp(LocalDateTime.now())
+				.code(HttpStatus.NOT_FOUND.value())
+				.build();
 	}
 	
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({ InvalidParameterException.class })
-	public List<String> handleInvalidParameter(Exception exception) {
+	public DetalheErro<String> handleInvalidParameter(Exception exception) {
+
 		String message = exception.getMessage();
-		log.info("Uma exception foi capturada {}", message);
-		return Arrays.asList(message);
+		log.info("Uma InvalidParameterException foi capturada {}", message);
+		
+		return new DetalheErro.Builder<String>()
+				.message(message)
+				.code(HttpStatus.NOT_FOUND.value())
+				.build();
 	}
 	
 }
